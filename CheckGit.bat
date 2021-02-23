ea8a56c41ec8a67e5abe6dd05b8fe11a4f30a8ab
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2021-02-22 23:30:42 -0500

    Delete unnecessary TODO

--- CheckGit.bat
+++ CheckGit.bat
@@ -7,7 +7,6 @@ REM It does this by recursively calling git status and git fetch on all director
 REM unsaved commits or new changes in the remote, the repo name is added to status.txt.
 REM ====================================================================================================
 
-REM TODO check that variables haven't already been taken
 
 SET out_file=status.txt
 SET no_repo="fatal: not a git repository (or any of the parent directories): .git"
