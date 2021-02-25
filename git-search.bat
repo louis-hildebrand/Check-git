6e97be8b6eb978ea257a353bbb5873b81c385147
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2021-02-24 23:10:11 -0500

    Add check for changes not staged for commit and changes in the remote

--- git-search.bat
+++ git-search.bat
@@ -6,11 +6,18 @@ REM ============================================================================
 
 SET temp_out=%~dp0
 SET temp_out=%temp_out%temp.txt
+
 SET no_repo="fatal: not a git repository (or any of the parent directories): .git"
 SET no_repo=%no_repo:"=%
+SET no_commits[0]="On branch master"
+SET no_commits[1]="Your branch is up-to-date with 'origin/master'."
+SET no_commits[2]="nothing to commit, working directory clean"
+
+SET /A status=0
 SET /A count=-1
 SET repos[0]=""
-SET output[0]=""
+SET has_commits[0]=""
+SET has_remote_changes[0]=""
 
 REM Parse arguments
 SET /A search_recursive=0
@@ -33,7 +40,7 @@ FOR %%i in (%*) DO (
 	REM If a path is given, start in that directory
 		IF !path_taken!==1 (
 			ECHO(
-			ECHO Fatal: git-search does not accept more than one path as arguments
+			ECHO [91mFatal: git-search does not accept more than one path as arguments[0m
 			EXIT /B 0
 		) ELSE (
 			ECHO Starting search in "!arg!"
@@ -42,14 +49,15 @@ FOR %%i in (%*) DO (
 		)
 	) ELSE (
 	REM Invalid argument
-		ECHO Warning: unused argument "!arg!"
+		ECHO [91mWarning: unused argument "!arg!"[0m
 	)
 )
 IF %path_taken%==0 (
 	ECHO Starting search in "%CD%"
 )
-ECHO(
-
+IF %show_progress%==1 (
+	ECHO(
+)
 REM Iterate through directories and check git status and git fetch
 IF %show_progress%==1 ( ECHO Progress: )
 FOR /D %%i in (./*) DO (
@@ -62,9 +70,17 @@ ECHO(
 IF %count%==-1 (
 	ECHO No git repositories found.
 ) ELSE (
-	ECHO Git repos:
+	SET /A count+=1
+	ECHO !count! git repos found:
+	SET /A count-=1
 	FOR /L %%i in (0, 1, %count%) DO (
 		ECHO     !repos[%%i]!
+		IF !has_commits[%%i]!==1 (
+			ECHO         [96mUnsaved commits[0m
+		)
+		IF !has_remote_changes[%%i]!==1 (
+			ECHO		[95mNew changes in the remote[0m
+		)
 	)
 )
 
@@ -81,24 +97,75 @@ REM ============================================================================
 :CheckDir
 	CD "%~dpnx1"
 	REM Get output from git status into output
-	GIT status > "%temp_out%" 2>&1
-	SET /A index=-1
-	FOR /F "eol=; tokens=* usebackq" %%i in ("%temp_out%") DO (
-		SET /A index+=1
-		SET output[!index!]="%%i"
-	)
-	REM Not a git repo
-	IF %output[0]%=="%no_repo%" (
-		REM Iterate through subdirectories
+	CALL :RunGitStatus status
+	IF %status%==0 (
+	REM Not a git repo: iterate through subdirectories if recursive option is selected
 		IF %search_recursive%==1 (
 			FOR /D %%i in (./*) DO (
 				CALL :CheckDir "%%i"
 			)
 		)
 	) ELSE (
-		REM Valid git repo
+	REM Valid git repo
 		SET /A count+=1
 		SET repos[!count!]="%~dpnx1"
+		IF %status%==2 (
+			SET /A has_commits[!count!]=1
+		) ELSE (
+			SET /A has_commits[!count!]=0
+		)
+		
+		REM Check for changes in the remote
+		CALL :RunGitFetch status
+		IF !status!==0 (
+			SET /A has_remote_changes[!count!]=0
+		) ELSE (
+			SET /A has_remote_changes[!count!]=1
+		)
 	)
 	CD ..
+EXIT /B 0
+
+REM Runs git status and returns an integer indicating the result:
+REM		0: Not a git repo
+REM		1: No changes or untracked files
+REM 	2: Changes to be committed or untracked files
+:RunGitStatus
+	GIT status > "%temp_out%" 2>&1
+	SET /A index=-1
+	FOR /F "eol=; tokens=* usebackq" %%i in ("%temp_out%") DO (
+		SET /A index+=1
+		SET output[!index!]="%%i"
+	)
+	REM Process output
+	IF %output[0]%=="%no_repo%" (
+	REM Not a git repo
+		SET /A %~1=0
+	) ELSE IF !index!==2 (
+	REM Possibly no commits
+		SET /A %~1=1
+	) ELSE (
+	REM Unsaved commits
+		SET /A %~1=2
+	)
+EXIT /B 0
+
+REM Runs git fetch and returns an integer indicating the result:
+REM		0: Nothing to fetch
+REM		1: new changes in the remote
+:RunGitFetch
+	GIT fetch > "%temp_out%" 2>&1
+	SET /A index=-1
+	FOR /F "eol=; tokens=* usebackq" %%i in ("%temp_out%") DO (
+		SET /A index+=1
+		SET output[!index!]="%%i"
+	)
+	REM Process output
+	IF !index!==-1 (
+	REM No message: nothing to fetch
+		SET /A %~1=0
+	) ELSE (
+	REM New changes in the remote
+		SET /A %~1=1
+	)
 EXIT /B 0
